89f5d5ace9944171acfede7987ccca4f7ad69822
blylei <blylei.info@gmail.com>
2023-09-22 08:39:08 +0800

    init

--- src/components/pages/contact/contact.tsx
+++ src/components/pages/contact/contact.tsx
@@ -131,7 +131,7 @@ const  Contact = () => {
                   id="message"
                   rows={4}
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-cyan-50 shadow-sm ring-1 ring-inset ring-cyan-50 placeholder:text-cyan-50 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={''}
+                  defaultValue=""
               />
                         </div>
                     </div>
