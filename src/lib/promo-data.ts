2556ec9989e64e3969163007469d8d7561f4ea7d
blylei <blylei.info@gmail.com>
2023-11-04 12:24:11 +0800

    init

--- src/lib/promo-data.ts
+++ src/lib/promo-data.ts
@@ -1,6 +1,6 @@
 const PROMO_DATA = {
     TOP_BANNER:{
-        title: 'Frabit platform v2.1.19 is already released, try it now',
+        title: 'Frabit v2.1.19 is already released, try it now',
         pathname: '/release-notes/',
         disabled: true,
     }
