f218f075c3ad9fd1c1ddebc51bbfcd144de76bab
blylei <blylei.info@gmail.com>
2023-12-10 17:43:24 +0800

    init

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -69,14 +69,14 @@ const Header = ({ hasBanner = false }: { hasBanner?: boolean }) => {
                                                     </div>
                                                 ))}
                                             </div>
-                                            <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                                            <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-cyan-500">
                                                 {callsToAction.map((item) => (
                                                     <a
                                                         key={item.name}
                                                         href={item.href}
-                                                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
+                                                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6  text-cyan-50"
                                                     >
-                                                        <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                                                        <item.icon className="h-5 w-5 flex-none text-cyan-50" aria-hidden="true" />
                                                         {item.name}
                                                     </a>
                                                 ))}
