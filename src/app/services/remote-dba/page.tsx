c9b5f0b569e7966bf91b163742eb1825b39d3087
blylei <blylei.info@gmail.com>
2024-01-06 15:31:59 +0800

    init

---
+++ src/app/services/remote-dba/page.tsx
@@ -0,0 +1,12 @@
+import RemoteDBA from '@/components/pages/services/remote-dba/remote_dba';
+
+const ContactUsPage = () => {
+    return (
+        <>
+            <h1 className="sr-only">Remote DBA</h1>
+            <RemoteDBA />
+        </>
+    );
+};
+
+export default ContactUsPage;
