5d10d20d04a17b48e6c5ced1fea682d866c60041
blylei <blylei.info@gmail.com>
2024-01-11 22:17:16 +0800

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,24 +19,13 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       keyframes: {
-        fadeIn: {
-          from: { opacity: 0 },
-          to: { opacity: 1 }
-        },
         marquee: {
           '0%': { transform: 'translateX(0%)' },
           '100%': { transform: 'translateX(-100%)' }
-        },
-        blink: {
-          '0%': { opacity: 0.2 },
-          '20%': { opacity: 1 },
-          '100% ': { opacity: 0.2 }
         }
       },
       animation: {
-        fadeIn: 'fadeIn .3s ease-in-out',
-        carousel: 'marquee 60s linear infinite',
-        blink: 'blink 1.4s both infinite'
+        carousel: 'marquee 60s linear infinite'
       }
     },
   },
